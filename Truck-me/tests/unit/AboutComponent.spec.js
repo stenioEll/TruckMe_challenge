8eb49e1dd1fc01877506ab2766fd63257faa689c
Stênio Ellison <39472541+stenioEll@users.noreply.github.com>
2024-07-14 17:42:33 -0300

    Testes unitários para Header e About Components usando Cypress, remoção de arquivos desnecessários.

---
+++ Truck-me/tests/unit/AboutComponent.spec.js
@@ -0,0 +1,26 @@
+import { mount } from '@cypress/vue';
+import AboutComponent from '@/components/AboutComponent.vue';
+
+describe('AboutComponent', () => {
+  it('deve exibir o título e subtítulo corretamente', () => {
+    mount(AboutComponent);
+
+    cy.get('.title h1').should('contain.text', 'Na Truck Me, somos especialistas em fretes.');
+
+    cy.get('.subtitle').should('contain.text', 'Uma startup que oferece soluções inovadoras para conectar embarcadores e transportadores de maneira eficiente e transparente de forma 100% digital.');
+  });
+
+  it('deve abrir e fechar o conteúdo adicional ao clicar no botão "Leia Mais"', () => {
+    mount(AboutComponent);
+
+    cy.get('#conteudo').should('not.have.class', 'conteudo-visivel');
+
+    cy.get('.btn-dark.btn-outline-dark').click();
+
+    cy.get('#conteudo').should('have.class', 'conteudo-visivel');
+
+    cy.get('.btn-dark.btn-outline-dark').click();
+
+    cy.get('#conteudo').should('not.have.class', 'conteudo-visivel');
+  });
+});
